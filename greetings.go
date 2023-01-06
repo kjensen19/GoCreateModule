13e135a59913e75cf8bac6291e251ae819111386
Kyle Jensen <kjensen19@gmail.com>
2023-01-05 22:33:02 -0600

    added error handling for no name entered

--- greetings.go
+++ greetings.go
@@ -1,13 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
-// ⬇			⬇️param	type	⬇️return type
-func Hello(name string) string {
+//
+//	⬇️param	type	⬇️return type
+func Hello(name string) (string, error) {
 	//⬆️Capital letter means a function can be called by a function not in the package(exported name)
 	//return a greeting that embeds the name
+	//If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	//	⬆️shortcut to declare and intialize
-	return message
+	return message, nil
 }
