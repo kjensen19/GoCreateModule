103835f2d8bda791e30cb9a31f52eccdb1c0bfcc
Kyle Jensen <kjensen19@gmail.com>
2023-01-05 23:12:52 -0600

    added random greeting function

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person
@@ -16,7 +18,26 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	//	⬆️shortcut to declare and intialize
 	return message, nil
+}
+
+// init sets intial values for variables in the function
+// init functions are executed at program start automatically, after global vars are intialized
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// ⬇️Lower case means it is only accessible in this package
+func randomFormat() string {
+	//A slice of messages
+	//			⬇️ Non-specified size is what lets Go know this is a slice
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	//specifies a random index for the slice
+	return formats[rand.Intn(len(formats))]
 }
